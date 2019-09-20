83770af056b07097f5f3844daf6d94f0d25fcebc
Luke Margolis <lukem1995@yahoo.com>
2019-09-20 09:50:26 -0400

    update login

--- Stage2/htmlGetter.py
+++ Stage2/htmlGetter.py
@@ -13,6 +13,28 @@ class HTMLGetter():
                 self.browser.set_handle_robots(False)
 		self.fileName = "myHTML.html"
 		self.domain = None
+		self.password = "None"
+		self.username = "None"
+
+	def setCredentials(self, username, password):
+		self.username = username
+		self.password = password
+
+	def login(self):
+		try:
+			pageBefore = self.browser.open(self.domain)
+			self.browser.select_form(nr=0)
+			self.browser["username"] = self.username
+			self.browser["password"] = self.password
+			self.browser.submit()
+			pageAfter = self.browser.open(self.domain)
+			if pageBefore == pageAfter:
+				print "Login failed. Try again"
+				exit()
+			print "Login successful"
+		except:
+			print "Login failed. Try again"
+			exit()
 
 	def getHTML(self, url):
 		self.domain = url
